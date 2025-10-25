b9d2d1bce558029ed89e59dac0ceffc8b5578f26
rafayghouri123 <rafayghouri50@gmail.com>
2025-10-25 20:16:37 +0500

    database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import {Button} from "@/components/ui/button";
+import prisma from "@/lib/database";
 export default function Page() {
+
+  const users = prisma.user.findMany();
+
   return (
     <div>
       <h1 className="text-red-500">Welcome home</h1>
-      <Button>Click me</Button>
+      {JSON.stringify(users)}
     </div>
   );
 }
