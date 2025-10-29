6ce1d64e022ed8e278105c8b0e86ad821231a032
rafayghouri123 <rafayghouri50@gmail.com>
2025-10-29 23:51:03 +0500

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import {Button} from "@/components/ui/button";
-import prisma from "@/lib/database";
-export default function Page() {
 
-  const users = prisma.user.findMany();
+import prisma from "@/lib/database";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { caller } from "@/trpc/server";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import Client from "./Client";
+
+export default async function  Page() {
+
+  const queryClient =getQueryClient()
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
 
   return (
     <div>
       <h1 className="text-red-500">Welcome home</h1>
-      {JSON.stringify(users)}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Client/>
+      </HydrationBoundary>
+      
     </div>
   );
 }
